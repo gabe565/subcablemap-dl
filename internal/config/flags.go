9331590185fb939c4ee83c059b90ae58ecef605c
Gabe Cook <gabe565@gmail.com>
2024-10-01 00:17:49 -0500

    chore(config): Add flag consts

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -6,15 +6,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	FlagYear        = "year"
+	FlagNoCrop      = "no-crop"
+	FlagTileMinX    = "tile-min-x"
+	FlagTileMaxX    = "tile-max-x"
+	FlagTileMinY    = "tile-min-y"
+	FlagTileMaxY    = "tile-max-y"
+	FlagZoom        = "zoom"
+	FlagParallelism = "parallelism"
+	FlagFormat      = "format"
+	FlagCompression = "compression"
+)
+
 func (c *Config) RegisterFlags(cmd *cobra.Command) {
-	cmd.Flags().IntVarP(&c.Year, "year", "y", c.Year, "Year to download (default latest available)")
-	cmd.Flags().BoolVarP(&c.NoCrop, "no-crop", "n", c.NoCrop, "Download the entire square map instead of cropping")
-	cmd.Flags().IntVar(&c.Tiles.Min.X, "tile-min-x", c.Tiles.Min.X, "X tile min (default determined by year and zoom)")
-	cmd.Flags().IntVar(&c.Tiles.Max.X, "tile-max-x", c.Tiles.Max.X, "X tile max (default determined by year and zoom)")
-	cmd.Flags().IntVar(&c.Tiles.Min.Y, "tile-min-y", c.Tiles.Min.Y, "Y tile min (default determined by year and zoom)")
-	cmd.Flags().IntVar(&c.Tiles.Max.Y, "tile-max-y", c.Tiles.Max.Y, "Y tile max (default determined by year and zoom)")
-	cmd.Flags().IntVarP(&c.Zoom, "zoom", "z", c.Zoom, "Zoom level")
-	cmd.Flags().IntVarP(&c.Parallelism, "parallelism", "p", c.Parallelism, "Number of goroutines to use")
-	cmd.Flags().StringVarP(&c.Format, "format", "f", c.Format, "Tile format. Try png, png8, png24. (default detected)")
-	cmd.Flags().VarP(&c.Compression, "compression", "c", "PNG compression level (one of "+strings.Join(CompressionLevelStrings(), ", ")+")")
+	cmd.Flags().IntVarP(&c.Year, FlagYear, "y", c.Year, "Year to download (default latest available)")
+	cmd.Flags().BoolVarP(&c.NoCrop, FlagNoCrop, "n", c.NoCrop, "Download the entire square map instead of cropping")
+	cmd.Flags().IntVar(&c.Tiles.Min.X, FlagTileMinX, c.Tiles.Min.X, "X tile min (default determined by year and zoom)")
+	cmd.Flags().IntVar(&c.Tiles.Max.X, FlagTileMaxX, c.Tiles.Max.X, "X tile max (default determined by year and zoom)")
+	cmd.Flags().IntVar(&c.Tiles.Min.Y, FlagTileMinY, c.Tiles.Min.Y, "Y tile min (default determined by year and zoom)")
+	cmd.Flags().IntVar(&c.Tiles.Max.Y, FlagTileMaxY, c.Tiles.Max.Y, "Y tile max (default determined by year and zoom)")
+	cmd.Flags().IntVarP(&c.Zoom, FlagZoom, "z", c.Zoom, "Zoom level")
+	cmd.Flags().IntVarP(&c.Parallelism, FlagParallelism, "p", c.Parallelism, "Number of goroutines to use")
+	cmd.Flags().StringVarP(&c.Format, FlagFormat, "f", c.Format, "Tile format. Try png, png8, png24. (default detected)")
+	cmd.Flags().VarP(&c.Compression, FlagCompression, "c", "PNG compression level (one of "+strings.Join(CompressionLevelStrings(), ", ")+")")
 }
