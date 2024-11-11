0df32a6300692f2b43912e8f992daf9c04528d60
Gabe Cook <gabe565@gmail.com>
2024-11-10 22:24:47 -0600

    feat: Set custom User-Agent header

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
 
 	"gabe565.com/subcablemap-dl/cmd"
 	"gabe565.com/utils/cobrax"
+	"gabe565.com/utils/httpx"
 )
 
 var version = "beta"
 
 func main() {
 	root := cmd.New(cobrax.WithVersion(version))
+	http.DefaultTransport = httpx.NewUserAgentTransport(nil, cobrax.BuildUserAgent(root))
 	if err := root.Execute(); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
