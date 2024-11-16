6aa8ab7da4f7be326204cef28740d31a2f332340
Gabe Cook <gabe565@gmail.com>
2024-11-16 00:57:14 -0600

    chore: Clean up flag error handling with must library

--- internal/config/completion.go
+++ internal/config/completion.go
@@ -1,42 +1,38 @@
 package config
 
 import (
-	"errors"
 	"strconv"
 	"time"
 
+	"gabe565.com/utils/must"
 	"github.com/spf13/cobra"
 )
 
 func (c *Config) RegisterCompletions(cmd *cobra.Command) {
-	if err := errors.Join(
-		cmd.RegisterFlagCompletionFunc(FlagYear, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return completeRange(2013, time.Now().Year())
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagTileMinX, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return completeRange(0, 63)
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagTileMaxX, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return completeRange(0, 63)
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagTileMinY, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return completeRange(0, 63)
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagTileMaxY, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return completeRange(0, 63)
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagZoom, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return completeRange(2, 6)
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagFormat, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return []string{"png", "png8", "png24"}, cobra.ShellCompDirectiveNoFileComp
-		}),
-		cmd.RegisterFlagCompletionFunc(FlagCompression, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return CompressionLevelStrings(), cobra.ShellCompDirectiveNoFileComp
-		}),
-	); err != nil {
-		panic(err)
-	}
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagYear, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return completeRange(2013, time.Now().Year())
+	}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagTileMinX, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return completeRange(0, 63)
+	}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagTileMaxX, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return completeRange(0, 63)
+	}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagTileMinY, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return completeRange(0, 63)
+	}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagTileMaxY, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return completeRange(0, 63)
+	}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagZoom, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return completeRange(2, 6)
+	}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagFormat, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return []string{"png", "png8", "png24"}, cobra.ShellCompDirectiveNoFileComp
+	}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagCompression, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return CompressionLevelStrings(), cobra.ShellCompDirectiveNoFileComp
+	}))
 }
 
 func completeRange(first, last int) ([]string, cobra.ShellCompDirective) {
